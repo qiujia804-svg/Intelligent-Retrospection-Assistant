1f5314ca15a0e616ba9ec0a050458f0a48775a6d
jiaqiu <qiujia804@gmail.com>
2026-02-18 10:51:05 +0800

    更新 Service Worker 版本至 v1.0.1，时间段改为半小时

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@
  * 提供离线缓存和后台同步功能
  */
 
-const CACHE_NAME = 'retrospection-assistant-v1.0.0';
-const STATIC_CACHE = 'static-v1.0.0';
-const DYNAMIC_CACHE = 'dynamic-v1.0.0';
+const CACHE_NAME = 'retrospection-assistant-v1.0.1';
+const STATIC_CACHE = 'static-v1.0.1';
+const DYNAMIC_CACHE = 'dynamic-v1.0.1';
 
 // 需要预缓存的静态资源
 const STATIC_ASSETS = [
